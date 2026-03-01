6d4f71aef01aa434903c10b6450f012048f0e1cd
Jørgen Galdal <jorgen.4@live.no>
2026-03-01 13:46:30 +0100

    fix: iterate over applicants in period instead of matched interviews (#423)
    * fix: iterate over applicants in period instead of matched interviews
    
    * fix imports
    
    * style: add break in email
    
    * fix typos

--- pages/admin/[period-id]/index.tsx
+++ pages/admin/[period-id]/index.tsx
@@ -32,7 +32,7 @@ const Admin = () => {
   useEffect(() => {
     setPeriod(data?.period);
     setCommittees(
-      data?.period.committees.concat(data?.period.optionalCommittees)
+      data?.period.committees.concat(data?.period.optionalCommittees),
     );
   }, [data, session?.user?.owId]);
 
@@ -67,7 +67,7 @@ const Admin = () => {
 
   const sendOutInterviewTimes = async ({ periodId }: { periodId: string }) => {
     const confirm = window.confirm(
-      "Er du sikker på at du vil sende ut intervju tider?"
+      "Er du sikker på at du vil sende ut intervjutider?",
     );
 
     if (!confirm) return;
@@ -77,7 +77,7 @@ const Admin = () => {
         `/api/periods/send-interview-times/${periodId}`,
         {
           method: "POST",
-        }
+        },
       );
       if (!response.ok) {
         throw new Error("Failed to send out interview times");
