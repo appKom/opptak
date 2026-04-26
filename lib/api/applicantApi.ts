63c0f620c6fe1fe95c506f69730fd7f0da3ae6f4
Jørgen Galdal <jorgen.4@live.no>
2026-04-26 17:18:30 +0200

    add fetchAllApplicants api route

--- lib/api/applicantApi.ts
+++ lib/api/applicantApi.ts
@@ -1,30 +1,37 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
 import { applicantType } from "../types/types";
+import { WithId } from "mongodb";
 
 export const fetchApplicantByPeriodAndId = async (
-  context: QueryFunctionContext
+  context: QueryFunctionContext,
 ) => {
   const periodId = context.queryKey[1];
   const applicantId = context.queryKey[2];
   return fetch(`/api/applicants/${periodId}/${applicantId}`).then((res) =>
-    res.json()
+    res.json(),
   );
 };
 
+export const fetchAllApplicants = async (
+  context: QueryFunctionContext,
+): Promise<WithId<applicantType>[] | undefined> => {
+  return fetch(`/api/applicants`).then((res) => res.json());
+};
+
 export const fetchApplicantsByPeriodId = async (
-  context: QueryFunctionContext
+  context: QueryFunctionContext,
 ) => {
   const periodId = context.queryKey[1];
   return fetch(`/api/applicants/${periodId}`).then((res) => res.json());
 };
 
 export const fetchApplicantsByPeriodIdAndCommittee = async (
-  context: QueryFunctionContext
+  context: QueryFunctionContext,
 ) => {
   const periodId = context.queryKey[1];
   const committee = context.queryKey[2];
   return fetch(`/api/committees/applicants/${periodId}/${committee}`).then(
-    (res) => res.json()
+    (res) => res.json(),
   );
 };
 
